c7b05ca1e8291ff74fa52caf0a4025d26591d38a
com0do99 <com0do99.net@gmail.com>
2015-08-10 11:53:34 +0700

    com0do99-net
    www.com0do99-net.com

---
+++ Source-tunnelGUI/Source-tunnelGUI/IMain.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Source_tunnelGUI
+{
+    partial class IMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPortHost = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPortHost);
+            this.groupBox1.Controls.Add(this.txtHost);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 42);
+            this.groupBox1.TabIndex = 121;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // txtPortHost
+            // 
+            this.txtPortHost.BackColor = System.Drawing.Color.White;
+            this.txtPortHost.Location = new System.Drawing.Point(203, 15);
+            this.txtPortHost.MaxLength = 4;
+            this.txtPortHost.Name = "txtPortHost";
+            this.txtPortHost.Size = new System.Drawing.Size(42, 21);
+            this.txtPortHost.TabIndex = 3;
+            // 
+            // txtHost
+            // 
+            this.txtHost.BackColor = System.Drawing.Color.White;
+            this.txtHost.Location = new System.Drawing.Point(78, 15);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(122, 21);
+            this.txtHost.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host     :";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 120);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(229, 32);
+            this.btnStart.TabIndex = 124;
+            this.btnStart.TabStop = false;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(3, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 15);
+            this.label3.TabIndex = 100;
+            this.label3.Text = "Username :";
+            // 
+            // txtusername
+            // 
+            this.txtusername.BackColor = System.Drawing.Color.White;
+            this.txtusername.Location = new System.Drawing.Point(78, 16);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(167, 21);
+            this.txtusername.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            this.txtPassword.Location = new System.Drawing.Point(78, 40);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(167, 21);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.txtPassword);
+            this.groupBox5.Controls.Add(this.txtusername);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Location = new System.Drawing.Point(3, 47);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(250, 67);
+            this.groupBox5.TabIndex = 123;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Authentication";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(3, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 15);
+            this.label4.TabIndex = 104;
+            this.label4.Text = "Password :";
+            // 
+            // IMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(257, 180);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox5);
+            this.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "IMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "com0do99.net - src";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPortHost;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Source-tunnelGUI/Source-tunnelGUI/IMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Source_tunnelGUI
+{
+    public partial class IMain : Form
+    {
+        private bool fStartStop;
+        private string command;
+        private string location = Application.StartupPath + "\\App\\BvSsh.exe";
+
+        public IMain()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Read more command
+         * https://www.bitvise.com/files/tunnelier-params.txt
+         */
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (this.fStartStop)
+            {
+                this.fStartStop = false;
+                this.btnStart.Text = "Start";
+
+                command = " -host=" + txtHost.Text + " -port=" + txtPortHost.Text + " -username=" + txtusername.Text
+                        + " -password=" + txtPassword.Text + " -loginOnStartup -title=[com0do99.net]SshTunnel" + " -proxy=n"
+                        + " -proxyUsername=" + txtusername.Text + " -proxyPassword=" + txtPassword.Text;
+                Process.Start(location, command);
+            }
+            else
+            {
+                this.fStartStop = true;
+                this.btnStart.Text = "Stop";
+                Stop("BvSsh.exe");
+            }
+        }
+
+        void Stop(string name)
+        {
+            Process[] pcs;
+            pcs = Process.GetProcessesByName(name);
+            if (pcs.Length > 0)
+                foreach (Process tn in pcs)
+                    tn.Kill();
+        }
+    }
+}
